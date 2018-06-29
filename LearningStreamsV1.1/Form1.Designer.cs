0105708c98500eae5a1566b76c1ab51013f3f0af
Hajk <tunczorx@gmail.com>
2018-06-29 11:25:15 +0200

    Using openFileDialog and saveFileDialog

--- LearningStreamsV1.1/Form1.Designer.cs
+++ LearningStreamsV1.1/Form1.Designer.cs
@@ -28,13 +28,72 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.saveButton = new System.Windows.Forms.Button();
+			this.openButton = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+			// 
+			// saveFileDialog1
+			// 
+			this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(12, 12);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(477, 157);
+			this.textBox1.TabIndex = 0;
+			// 
+			// saveButton
+			// 
+			this.saveButton.Location = new System.Drawing.Point(414, 175);
+			this.saveButton.Name = "saveButton";
+			this.saveButton.Size = new System.Drawing.Size(75, 23);
+			this.saveButton.TabIndex = 2;
+			this.saveButton.Text = "Zapisz";
+			this.saveButton.UseVisualStyleBackColor = true;
+			this.saveButton.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// openButton
+			// 
+			this.openButton.Location = new System.Drawing.Point(333, 175);
+			this.openButton.Name = "openButton";
+			this.openButton.Size = new System.Drawing.Size(75, 23);
+			this.openButton.TabIndex = 1;
+			this.openButton.Text = "Otwórz";
+			this.openButton.UseVisualStyleBackColor = true;
+			this.openButton.Click += new System.EventHandler(this.saveButton_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(501, 203);
+			this.Controls.Add(this.saveButton);
+			this.Controls.Add(this.openButton);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button saveButton;
+		private System.Windows.Forms.Button openButton;
 	}
 }
 
--- LearningStreamsV1.1/Form1.cs
+++ LearningStreamsV1.1/Form1.cs
@@ -7,14 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace LearningStreamsV1._1
 {
 	public partial class Form1 : Form
 	{
+		string name;
 		public Form1()
 		{
 			InitializeComponent();
+
+		}
+
+		private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+		{
+
+		}
+
+		private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+		{
+
+		}
+
+		private void saveButton_Click(object sender, EventArgs e)//otwórz
+		{
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				name = openFileDialog1.FileName;
+				textBox1.Clear();
+				textBox1.Text = File.ReadAllText(name);
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)//zapisz
+		{
+			if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				name = saveFileDialog1.FileName;
+				File.WriteAllText(name, textBox1.Text);
+			}
 		}
 	}
 }
