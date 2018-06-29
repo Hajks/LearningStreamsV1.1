e5e64dca6b5dbfb75bdc5229b400ad25734a1502
Hajk <tunczorx@gmail.com>
2018-06-29 11:31:45 +0200

    Form1 fixed

--- LearningStreamsV1.1/Form1.Designer.cs
+++ LearningStreamsV1.1/Form1.Designer.cs
@@ -33,6 +33,10 @@
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.saveButton = new System.Windows.Forms.Button();
 			this.openButton = new System.Windows.Forms.Button();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.flowLayoutPanel1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// openFileDialog1
@@ -46,15 +50,16 @@
 			// 
 			// textBox1
 			// 
-			this.textBox1.Location = new System.Drawing.Point(12, 12);
+			this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.textBox1.Location = new System.Drawing.Point(3, 3);
 			this.textBox1.Multiline = true;
 			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(477, 157);
+			this.textBox1.Size = new System.Drawing.Size(495, 162);
 			this.textBox1.TabIndex = 0;
 			// 
 			// saveButton
 			// 
-			this.saveButton.Location = new System.Drawing.Point(414, 175);
+			this.saveButton.Location = new System.Drawing.Point(417, 3);
 			this.saveButton.Name = "saveButton";
 			this.saveButton.Size = new System.Drawing.Size(75, 23);
 			this.saveButton.TabIndex = 2;
@@ -64,7 +69,7 @@
 			// 
 			// openButton
 			// 
-			this.openButton.Location = new System.Drawing.Point(333, 175);
+			this.openButton.Location = new System.Drawing.Point(336, 3);
 			this.openButton.Name = "openButton";
 			this.openButton.Size = new System.Drawing.Size(75, 23);
 			this.openButton.TabIndex = 1;
@@ -72,18 +77,44 @@
 			this.openButton.UseVisualStyleBackColor = true;
 			this.openButton.Click += new System.EventHandler(this.saveButton_Click);
 			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 1;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 1);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 2;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 83.25123F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.74877F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(501, 203);
+			this.tableLayoutPanel1.TabIndex = 3;
+			// 
+			// flowLayoutPanel1
+			// 
+			this.flowLayoutPanel1.Controls.Add(this.saveButton);
+			this.flowLayoutPanel1.Controls.Add(this.openButton);
+			this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 171);
+			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(495, 29);
+			this.flowLayoutPanel1.TabIndex = 1;
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(501, 203);
-			this.Controls.Add(this.saveButton);
-			this.Controls.Add(this.openButton);
-			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.tableLayoutPanel1);
 			this.Name = "Form1";
 			this.Text = "Form1";
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.flowLayoutPanel1.ResumeLayout(false);
 			this.ResumeLayout(false);
-			this.PerformLayout();
 
 		}
 
@@ -94,6 +125,8 @@
 		private System.Windows.Forms.TextBox textBox1;
 		private System.Windows.Forms.Button saveButton;
 		private System.Windows.Forms.Button openButton;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
 	}
 }
 
